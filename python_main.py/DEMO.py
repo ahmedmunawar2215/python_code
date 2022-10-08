2fcabfa43ec30a408f2217ec90e2ce799a310030
Ibrahim <Ibrahim@Ibrahims-MacBook-Air.local>
2022-10-08 21:52:18 +0300

    test code

--- python_main.py/DEMO.py
+++ python_main.py/DEMO.py
@@ -1,1 +1,15 @@
 #PYTHON PROJECT
+
+city_list = []
+counter = True
+
+while counter == True:
+    var = input("Name of city you know in Turkey:\n")
+    city_list.append(var)
+    ask_user = input("Do you know more:\n[Y/N])").lower()
+    if ask_user == "n":
+        counter = False
+        
+
+print(city_list)
+    
